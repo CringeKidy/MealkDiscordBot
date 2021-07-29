2e7664b4857c4e8fd9963123c93df48d15eef0bc
CringeKidy <justacecornelder15@gmail.com>
2021-07-30 00:17:10 +1200

    updating backend

--- index.js
+++ index.js
@@ -1,42 +1,21 @@
 require('dotenv').config();
-require('./strats/discord.js')
 
 // server/index.js
 const express = require('express');
 const PORT = process.env.PORT || 3001;
-const apiwebsite = process.env.URL || "http://localhost:3001"
 
-const path = require('path');
-const routes = require('./routes')
-
-const ConnectionURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@data.3nr62.mongodb.net/Mealk`
-
-const passport = require('passport');
-const session = require('express-session');
-const cors = require('cors')
-const Store  = require('connect-mongo');
+const cors = require('cors');
 
 const app = express();
+
 app.use(cors({
-  origin:[apiwebsite],
-  credentials: true,
+  origin:'http://localhost:3000'
 }))
 
-app.use(session({
-  secret: process.env.SECRET,
-  cookie:{
-    maxAge: 60000 * 60 * 24
-  },
-  resave: false,
-  saveUninitialized: false,
-  store: Store.create({
-    mongoUrl: ConnectionURL
+app.get('/api', (req, res) => {
+  res.send({msg:"yes"})
 })
-}))
-app.use(passport.initialize());
-app.use(passport.session());
 
-app.use('/api', routes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
