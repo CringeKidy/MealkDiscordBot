0dd50bcce1d4de21d557eb5727ef45edb067a5b8
CringeKidy <justacecornelder15@gmail.com>
2021-08-04 20:28:10 +1200

    little bit more frontend work

--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -1,27 +1,14 @@
-import {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
-const apiUrl = process.env.API_URL || 'http://localhost:3001'
+const loginUrl = process.env.loginURL || "http://localhost:3001"
 
 export default function LandingPage(){
-    const [apiData, setApiData] = useState()
-    
-    useEffect(() => {
-        FetchApiData()
-    })
-    
-    const FetchApiData = async () => {
-        const res = await fetch(`${apiUrl}/api`)
-        const data = await res.json()
-        setApiData(data)
-    }
-
     return(
         <div>
+            <button onClick={this.window.href = loginUrl+"/api/auth/discord"}>Login</button>
             <h1>Landing page</h1>
             <Link to="/menu">
                 <p>Menu page</p>
             </Link>
-            <p>{apiData?.msg}</p>
         </div>
     )
 }
